2f91c31d139e1c01b16c599d2f9ae9d1ec51d7f6
Nan Wang <wanghaonanlpc@gmail.com>
2020-08-07 11:26:45 -0400

    add ch03

---
+++ 002数据结构与算法python进阶/practice/5.28-30/1.0.3fibonaci.py
@@ -0,0 +1,27 @@
+# fibonaci squence is the series of numbers:
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+
+def fibonacci1(n):
+    assert(n>=0)
+    a, b = 0, 1
+    for i in range(1, n+1):
+        a, b = b, a + b
+    return a    
+    
+def fibonacci2(n):
+    assert(n>=0)
+    if (n <= 2): 
+        return 1
+    return fibonacci2(n-1) + fibonacci2(n-2)
+
+def fibonacci3(n):
+    assert(n>=0)
+    if (n <= 1): 
+        return (n,0)
+    (a, b) = fibonacci3(n-1)
+    return (a+b, a)
+
+
+
+print(fibonacci1(10),fibonacci2(10),fibonacci3(10))
