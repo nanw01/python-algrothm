750a50db7dc6ebba4a228a31ff09a4d748cceba2
Nan Wang <wanghaonanlpc@gmail.com>
2020-08-09 09:44:03 -0400

    search_insert_position

--- 002数据结构与算法python进阶/practice/0506/050105searchinsertposition.py
+++ 002数据结构与算法python进阶/practice/0506/050105searchinsertposition.py
@@ -1,12 +1,33 @@
-# Given a sorted array and a target value, 
-# return the index if the target is found. 
-# If not, return the index where it would be if it were inserted in order. 
+# Given a sorted array and a target value,
+# return the index if the target is found.
+# If not, return the index where it would be if it were inserted in order.
 # You may assume no duplicates in the array
 
 
 def search_insert_position(alist, target):
-    pass
+    if len(alist) == 0:
+        return 0
+
+    left, right = 0, len(alist)-1
+
+    while left+1 < right:
+
+        mid = left+(right-left)//2
+
+        if alist[mid] == target:
+            return mid
+        if alist[mid] < target:
+            left = mid
+        else:
+            right = mid
+
+    if alist[left] >= target:
+        return left
+    if alist[right] >= target:
+        return right
+
+    return right+1
 
 
 num_list = [5, 7, 9]
-print(search_insert_position(num_list,8))
+print(search_insert_position(num_list, 10))
