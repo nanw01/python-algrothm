44e4679b7c4cbdfb0d59e1c7ae8a7993a8050c98
Nan Wang <wanghaonanlpc@gmail.com>
2020-08-08 20:51:35 -0400

    binary

--- 002数据结构与算法python进阶/practice/0506/050103roatedsord.py
+++ 002数据结构与算法python进阶/practice/0506/050103roatedsord.py
@@ -1,27 +1,25 @@
 # 1.0.3  Ex.3 Find Min in Rotated Sorted Array
 # Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand. Find the minimum element.
 
-def search(lst):
-    if len(lst) == 0:
+def search(alist):
+    if len(alist) == 0:
         return -1
 
-    left = 0
-    right = len(lst)-1
+    left, right = 0, len(alist)-1
 
-    while left + 1 < right:
+    while left+1 < right:
+        if alist[left] < alist[right]:
+            return alist[left]
 
-        if lst[left]<lst[right]:
-            return lst[left]
+        mid = left + (right-left)//2
 
-        mid = left+(right-left)//2
-
-        if left < mid:
-            left = mid
-        elif left > mid: 
+        if alist[mid] >= alist[left]:
+            left = mid+1
+        else:
             right = mid
 
-    return lst[left] if lst[left] < lst[right] else lst[right]
+    return alist[left] if alist[left] < alist[right] else alist[right]
 
 
-num_list = [10,1,2,3,5,7,8,9]
+num_list = [10, 22, 33, 5, 7, 8, 9]
 print(search(num_list))
