875d3abd35cc4941b38059fe6dc24d7d9eb45e4e
wanghaonanlpc <wanghaonanlpc@gmail.com>
2021-01-21 12:19:28 -0500

    0121

--- laioffer/notes/quicksort.py
+++ laioffer/notes/quicksort.py
@@ -1,33 +1,21 @@
 class Solution:
-    from random import randrange
 
-    def partition(self, lst, start, end, pivot_index):
-        lst[pivot_index], lst[end] = lst[end], lst[pivot_index]
+    def quickSortHelper(self, lst, left, right):
+        """
+        docstring
+        """
+        from random import randrange
+        if len(lst) <= 1:
+            return lst
 
-        store_index = start
-        pivot = lst[end]
-
-        for i in range(start, end):
-            if lst[i] < pivot:
-                lst[i], lst[store_index] = lst[store_index], lst[i]
-                store_index += 1
-
-        lst[store_index], lst[end] = lst[end], lst[store_index]
-
-        return store_index
-
-    def quick_sort_helper(self, lst, start, end):
-        if start >= end:
-            return
-        pivot_index = self.randrange(start, end+1)
-        new_pivot_index = self.partition(lst, start, end, pivot_index)
-        self.quick_sort_helper(lst, start, new_pivot_index-1)
-        self.quick_sort_helper(lst, new_pivot_index+1, end)\
-
+        pivot_index = randrange(left, right)
+        new_pivot_index = self.partition(lst, left, right, pivot_index)
 
     def quickSort(self, lst):
-        self.quick_sort_helper(lst, 0, len(lst)-1)
-        return lst
+        """
+        docstring
+        """
+        self.quickSortHelper(lst, 0, len(lst)-1)
 
 
 print(Solution().quickSort([2, 1, 3, 6, 4, 5, 9, 7, 8, 0]))
