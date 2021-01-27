cbf0d21dea83a959fd634f1ca7c64ecf1727866d
wanghaonanlpc <wanghaonanlpc@gmail.com>
2021-01-27 13:33:57 -0500

    012700

--- laioffer/Code/613. Design Circular Deque.py
+++ laioffer/Code/613. Design Circular Deque.py
@@ -1,94 +1,48 @@
 class Solution(object):
-
     def __init__(self, k):
-        """
-        Initialize your data structure here. Set the size of the deque to be k.
-        :type k: int
-        """
-        self.queue = []
-        self.size = k
-        self.front = 0
-        self.rear = 0
+        self._size = 0
+        self._items = [None]*(k+1)
+        self._first = 0
+        self._last = 1
 
     def insertFront(self, value):
-        """
-        Adds an item at the front of Deque. Return true if the operation is successful.
-        :type value: int
-        :rtype: bool
-        """
-        if not self.isFull():
-            self.queue.insert(0, value)
-            self.rear += 1
-            return True
-        else:
+        if self.isFull():
             return False
+        self._items[self._first] = value
+        self._first = (self._first - 1 + len(self._items)) % len(self._items)
+        self._size += 1
+        return True
 
     def insertLast(self, value):
-        """
-        Adds an item at the rear of Deque. Return true if the operation is successful.
-        :type value: int
-        :rtype: bool
-        """
-        if not self.isFull():
-            self.queue.append(value)
-            self.rear += 1
-            return True
-        else:
+        if self.isFull():
             return False
+        self._items[self._first] = value
+        self._first = (self._last + 1) % len(self._items)
+        self._size += 1
+        return True
 
     def deleteFront(self):
-        """
-        Deletes an item from the front of Deque. Return true if the operation is successful.
-        :rtype: bool
-        """
+        if self.isEmpty():
+            return False
+        self._first = (self._first + 1) % len(self._items)
+        self._size -= 1
+        return True
 
     def deleteLast(self):
-        """
-        Deletes an item from the rear of Deque. Return true if the operation is successful.
-        :rtype: bool
-        """
+        if self.isEmpty():
+            return False
+        self._last = (self._last - 1 + len(self._items)) % len(self._items)
+        self._size -= 1
+        return True
 
     def getFront(self):
-        """
-        Get the front item from the deque.
-        :rtype: int
-        """
+        return -1 if self.isEmpty() else self._items[self._first + 1 % len(self._items)]
 
     def getRear(self):
-        """
-        Get the last item from the deque.
-        :rtype: int
-        """
+        return -1 if self.isEmpty() else self._items[(self._last - 1 + len(self._items)) % len(self._items)]
 
     def isEmpty(self):
-        """
-        Checks whether the circular deque is empty or not.
-        :rtype: bool
-        """
+        return self._size == 0
 
     def isFull(self):
-        """
-        Checks whether the circular deque is full or not.
-        :rtype: bool
-        """
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(3)
-# param_1 = obj.insertFront(1)
-# param_2 = obj.insertLast(2)
-# param_3 = obj.deleteFront()
-# param_4 = obj.deleteLast()
-# param_5 = obj.getFront()
-# param_6 = obj.getRear()
-# param_7 = obj.isEmpty()
-# param_8 = obj.isFull()
-
-# print(param_1)
-# print(param_2)
-# print(param_3)
-# print(param_4)
-# print(param_5)
-# print(param_6)
-# print(param_7)
-# print(param_8)
+        return self._size == len(self._items) - 1
