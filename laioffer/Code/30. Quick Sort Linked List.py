e996d0b5f87fbd4d9bb69e5b7484825d61b8060b
wanghaonanlpc <wanghaonanlpc@gmail.com>
2021-02-19 19:49:32 -0500

    0219020

--- laioffer/Code/30. Quick Sort Linked List.py
+++ laioffer/Code/30. Quick Sort Linked List.py
@@ -12,7 +12,90 @@ class Solution(object):
         return: ListNode
         """
         # write your solution here
-        if head is None or head.next is None:
+        if head is None:
             return head
 
-        pivot = head.val
+        tail = self.getTail(head)
+        head, tail = self._quickSort(head, tail)
+        tail.next = None
+
+        return head
+
+    def _quickSort(self, head, tail):
+        if head is not tail:
+            headLeft, tailLeft, headRef, tailRef, headRight, tailRight = self._quickSortPartition(
+                head, tail)
+            if headLeft is None:
+                head = headRef
+            else:
+                headLeft, tailLeft = self._quickSort(headLeft, tailLeft)
+                head = headLeft
+                tailLeft.next = headRef
+            if headRight is None:
+                tail = tailRef
+            else:
+                headRight, tailRight = self._quickSort(headRight, tailRight)
+                tailRef.next = headRight
+                tail = tailRight
+        return head, tail
+
+    def _quickSortPartition(self, head, tail):
+        reference = tail
+        headRef, tailRef = reference, reference
+        headLeft, tailLeft, headRight, tailRight = None, None, None, None
+
+        sentinel = ListNode(None)
+        sentinel.next = head
+        node = sentinel
+        while node.next is not tail:
+            node = node.next
+            if node.val > reference.val:
+                if headRight is not None:
+                    tailRight.next = node
+                    tailRight = node
+                else:
+                    headRight = node
+                    tailRight = node
+            elif node.val < reference.val:
+                if headLeft is not None:
+                    tailLeft.next = node
+                    tailLeft = node
+                else:
+                    headLeft = node
+                    tailLeft = node
+            else:
+                tailRef.next = node
+                tailRef = node
+
+        return headLeft, tailLeft, headRef, tailRef, headRight, tailRight
+
+    def getTail(self, node):
+        while node.next:
+            node = node.next
+
+        return node
+
+
+def printList(node):
+    a = []
+    while node is not None:
+        a.append(node.val)
+        node = node.next
+    return a
+
+
+def buildList(seq):
+    f = ListNode(None)
+    c = f
+    for i in seq:
+        c.next = ListNode(i)
+        c = c.next
+
+    return f.next
+
+
+node = buildList([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
+print(printList(node))
+
+head = Solution().quickSort(node)
+print(printList(head))
