d156067994e33fab776c59cf6c86d72835416651
_lpc <wanghaonanlpc@users.noreply.github.com>
2021-04-15 23:10:09 -0400

    0415

--- laioffer/Code/617. First Unique Character in a String.py
+++ laioffer/Code/617. First Unique Character in a String.py
@@ -1,3 +1,24 @@
+# class Solution(object):
+#     def firstUniqChar(self, input):
+#         """
+#         input: string input
+#         return: int
+#         """
+#         # write your solution here
+
+#         from collections import Counter
+
+#         # counts = Counter(input)
+#         # for i in input:
+#         #     if counts.get(i) == 1:
+#         #         return input.index(i)
+#         # return -1
+
+#         counts = Counter(input)
+#         for i, j in enumerate(input):
+#             if counts[j] == 1:
+#                 return i
+#         return -1
 class Solution(object):
     def firstUniqChar(self, input):
         """
@@ -5,22 +26,20 @@ class Solution(object):
         return: int
         """
         # write your solution here
-
-        from collections import Counter
-
-        # counts = Counter(input)
-        # for i in input:
-        #     if counts.get(i) == 1:
-        #         return input.index(i)
-        # return -1
-
-        counts = Counter(input)
-        for i, j in enumerate(input):
-            if counts[j] == 1:
+        hash = dict()
+        for i in input:
+            if i not in hash:
+                hash[i] = 0
+            else:
+                hash[i] += 1
+        print(hash)
+        for i in range(len(input)):
+            if hash[input[i]] == 0:
                 return i
+
         return -1
 
 
 s = Solution()
-ss = "vftdbnkzzzksjxxawxuwdelcjzpjnrloxyfsohopsipoubukmhlpssauyxhxnpxajpufkrxmxckduktenozsffoprjvhstl"
+ss = "viv"
 print(s.firstUniqChar(ss))
