843a9f156a1175557934521a0d8526f07bb3abb8
Nan Wang <wanghaonanlpc@gmail.com>
2021-04-28 21:52:49 -0400

    042802

--- LeetCode-SQL/_578.sql
+++ LeetCode-SQL/_578.sql
@@ -1,10 +1,21 @@
-SELECT question_id AS 'survey_log'
-FROM survey_log
-GROUP BY question_id
-ORDER BY COUNT(answer_id) / COUNT(
+-- SELECT question_id AS 'survey_log'
+-- FROM survey_log
+-- GROUP BY question_id
+-- ORDER BY COUNT(answer_id) / COUNT(
+--         case
+--             when survey_log.action = 'show' then survey_log.action
+--             else null
+--         end
+--     ) DESC
+-- LIMIT 0, 1;
+# Write your MySQL query statement below
+select s.question_id as survey_log
+from survey_log as s
+group by s.question_id
+order by count(s.answer_id) / count(
         case
-            when survey_log.action = 'show' then survey_log.action
+            when s.action = 'show' then s.action
             else null
-        end
+        END
     ) DESC
-LIMIT 0, 1
+limit 0, 1
--- LeetCode-SQL/_580.sql
+++ LeetCode-SQL/_580.sql
@@ -1,7 +1,8 @@
+# Write your MySQL query statement below
 select d.dept_name,
     count(s.student_id) as student_number
-from department d
-    left join student s on d.dept_id = s.dept_id
-group by d.dept_name
+from department as d
+    left join student as s using(dept_id)
+group by d.dept_id
 order by student_number desc,
     d.dept_name;
