37187dde9904404de57db24d496fbe5d0da60ba0
Nan Wang <wanghaonanlpc@gmail.com>
2021-04-28 22:08:38 -0400

    042802

--- LeetCode-SQL/_585.sql
+++ LeetCode-SQL/_585.sql
@@ -13,3 +13,20 @@ where (
             where a.TIV_2015 = c.TIV_2015
         )
     );
+# Write your MySQL query statement below
+select sum(TIV_2016) as TIV_2016
+from insurance
+where TIV_2015 in(
+        select TIV_2015
+        from insurance
+        group by TIV_2015
+        having count(*) >= 2
+    )
+    and (lat, lon) in(
+        select lat,
+            lon
+        from insurance
+        group by lat,
+            lon
+        having count(*) = 1
+    )
--- LeetCode-SQL/_586.sql
+++ LeetCode-SQL/_586.sql
@@ -1,5 +1,5 @@
-SELECT customer_number
-FROM orders
-GROUP BY customer_number
+SELECT o.customer_number
+FROM orders as o
+GROUP BY o.customer_number
 ORDER BY COUNT(*) DESC
 LIMIT 1;
