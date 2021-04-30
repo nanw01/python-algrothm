d693c83bed3355988366cb42054d9ff7e8018593
Nan Wang <wanghaonanlpc@gmail.com>
2021-04-30 16:04:20 -0400

    043000

--- LeetCode-SQL/_603.sql
+++ LeetCode-SQL/_603.sql
@@ -14,20 +14,20 @@ where c.free = 1
         )
     )
 order by c.seat_id;
-# Write your MySQL query statement below
-select c.seat_id
-from cinema as c
-where c.free = 1
-    and (
-        c.seat_id - 1 in (
-            select seat_id
-            from cinema
-            where free = 1
-        )
-        or c.seat_id + 1 in (
-            select seat_id
-            from cinema
-            where free = 1
-        )
-    )
-order by c.seat_id
+-- # Write your MySQL query statement below
+-- select c.seat_id
+-- from cinema as c
+-- where c.free = 1
+--     and (
+--         c.seat_id - 1 in (
+--             select seat_id
+--             from cinema
+--             where free = 1
+--         )
+--         or c.seat_id + 1 in (
+--             select seat_id
+--             from cinema
+--             where free = 1
+--         )
+--     )
+-- order by c.seat_id
