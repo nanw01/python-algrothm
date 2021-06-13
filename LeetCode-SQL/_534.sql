17e5b0f09ce8b33fc0270a94c7be93a9cbaee2ce
Nan Wang <wanghaonanlpc@gmail.com>
2021-06-13 09:36:17 -0700

    061300

--- LeetCode-SQL/_534.sql
+++ LeetCode-SQL/_534.sql
@@ -1,3 +1,4 @@
+# Write your MySQL query statement below
 select a1.player_id,
     a1.event_date,
     sum(a2.games_played) as games_played_so_far
@@ -6,3 +7,13 @@ from Activity a1
     and a1.event_date >= a2.event_date
 group by a1.player_id,
     a1.event_date;
+# Write your MySQL query statement below
+select a1.player_id,
+    a1.event_date,
+    sum(a2.games_played) as games_played_so_far
+from activity a1,
+    activity a2
+where a1.player_id = a2.player_id
+    and a1.event_date >= a2.event_date
+group by a1.player_id,
+    a1.event_date
